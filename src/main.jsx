b25654d10230b35583d96423901abecbaf53b57e
Nehahere12 <125659761+Nehahere12@users.noreply.github.com>
2024-06-29 10:31:11 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {BrowserRouter} from "react-router-dom"
-import StorecontextProvider from './Context/Storecontext.jsx'
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cheers-Say hi to drinks</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <!-- Ensure the correct relative path to main.jsx -->
+    <script type="module" src="/src/main.jsx"></script>
+  </body>
+</html>
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- <BrowserRouter>
- <StorecontextProvider>
- <App />
- </StorecontextProvider>
-    
-    </BrowserRouter>
- 
-)
